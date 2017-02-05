de5326202a944a8aab98341648113208f2238800
Bryan Brophy <brybrophy@gmail.com>
2017-02-05 01:16:43 -0800

    Work on typography scss

--- src/components/home/HomeView.jsx
+++ src/components/home/HomeView.jsx
@@ -9,6 +9,13 @@ export default class HomeView extends Component {
 		return (
 			<div className="page home">
 				<h1>{store.welcomeMessage}</h1>
+				<h2>{store.welcomeMessage}</h2>
+				<h3>{store.welcomeMessage}</h3>
+				<h4>{store.welcomeMessage}</h4>
+				<p>{store.welcomeMessage}</p>
+				<h5>{store.welcomeMessage}</h5>
+				<h6>{store.welcomeMessage}</h6>
+				<small>{store.welcomeMessage}</small>
 			</div>
 		);
 	}
