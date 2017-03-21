5a053b62e0fc2ab12091ce96805a7249978d8451
Bryan Brophy <brybrophy@gmail.com>
2017-03-21 15:53:24 -0700

    Change Class Name

--- server/models/example.js
+++ server/models/example.js
@@ -1,8 +1,8 @@
 const { bookshelf } = require('../../orm');
 
-const Category = bookshelf.Model.extend({
+const Example = bookshelf.Model.extend({
   tableName: 'example',
   hasTimestamps: true
 });
 
-module.exports = Category;
+module.exports = Example;
