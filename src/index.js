bd9dde36e89b6d556db8aea79a7756741d2fe090
Yohai Rosen <yohai.roz@gmail.com>
2018-01-07 17:51:42 +0200

    testing...

--- src/index.js
+++ src/index.js
@@ -62,13 +62,29 @@ const stores = {
     surveyStore
 };
 
+function toggleFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+    }
+    else {
+        cancelFullScreen.call(doc);
+    }
+}
+
 window.addEventListener("load",function() {
     // Set a timeout...
     console.log('started')
     setTimeout(function(){
         // Hide the address bar!
         console.log('loaded')
-        window.scrollTo(0, 1);
+        toggleFullScreen()
+        // window.scrollTo(0, 1);
     }, 0);
 });
 
