f1878357ea48bcaa7de4710708317528c227f531
Yohai Rosen <yohai.roz@gmail.com>
2018-07-12 17:27:15 +0300

    cosmetic changes

--- src/components/editor/SurveyEditor.js
+++ src/components/editor/SurveyEditor.js
@@ -1,39 +1,55 @@
 import React, {Component} from 'react';
 import * as SurveyJSEditor from 'surveyjs-editor';
+import * as Survey from 'survey-react';
 
 class SurveyEditor extends Component {
 
     componentDidMount() {
-        const {surveyId} = this.props;
-
-
+        //     this.editor.loadSurvey(surveyId)
         let defs = SurveyJSEditor
             .SurveyQuestionEditorDefinition
             .definition;
-        //     this.editor.loadSurvey(surveyId)
 
 
+        const {surveyId} = this.props;
+
+        defs["rating"].tabs = [{name: 'rateValues', title: 'Rating values definitions', index: 1}]
+
+        // console.log(defs["rating"].tabs )
         defs["questionbase"].tabs = [{name: 'visibleIf', title: 'Visible If (Coming Soon)', index: 100}]
         defs["questionbase"]
-            .properties = ['name', 'description','isRequired'];
+            .properties = [
+            {name: 'name', title: 'Question text'},
+            'isRequired'];
 
 
         defs["text"]
-            .properties = ['placeHolder'];
+            .properties = [{name: 'description', title: 'Additional info (if applicable)'}, {
+            name: 'placeHolder',
+            title: 'Sample answer (if applicable)'
+        }];
 
         defs["selectbase"]
-            .properties = [{name: 'hasOther', 'title': 'Add "Other" option'}, {
-            name: 'hasComment',
-            'title': 'Add "Other" option with comment'
-        }];
+            .properties = [
+            {name: 'hasOther', 'title': 'Add "Other" option'},
+            {name: 'hasComment', 'title': 'Add "Other" option with comment'},
+            {name: 'description', title: 'Further instructions'},
+            {
+                name: 'chocies', properties: [
+                {name: 'itemvalue', properties: [{name: 'text', visible: false}]}
+
+            ]
+            }
+        ];
 
         defs["boolean"]
             .properties = [];
 
-        defs["rating"].properties = [{name: 'rateMin', title: 'Min Rate Value'}, {
-            name: 'rateMax',
-            title: 'Max Rate Value'
-        }];
+        defs["rating"].properties = [
+            {name: 'rateMin', title: 'Min Rate Value'}, {
+                name: 'rateMax',
+                title: 'Max Rate Value'
+            }];
 
         let editorOptions = {
             showEmbededSurveyTab: false, showJSONEditorTab: false, showPropertyGrid: false,
@@ -41,6 +57,7 @@ class SurveyEditor extends Component {
         };
 
         this.editor = new SurveyJSEditor.SurveyEditor('surveyEditorContainer', editorOptions);
+        console.log(this.editor)
         this.editor.loadSurvey(surveyId);
         this.editor.isAutoSave = true;
 
@@ -79,14 +96,50 @@ class SurveyEditor extends Component {
 
 
 // this.propsToHide = ['startWithNewLine', 'visible', 'isRequired', 'visibleIf', 'title', 'inputType', 'choicesOrder', 'colCount', 'choicesByUrl'];
-        this.propsToHide = ['visibleIf', 'choicesByUrl', 'rateValues'];
+//         this.propsToHide = ['visibleIf', 'choicesByUrl', 'rateValues'];
 //
-//
+
+
+        this.editor.survey.requiredText = "";
+
+
         this.editor.onQuestionAdded.add((editor, options) => {
             options.question.isRequired = true;
+
+            var t = options.question.getType();
+            options.question.title = 'Q' + options.question.title.slice(1);
+            // options.question.requiredText = '';
+
+
+            console.log(options.question)
+            if (['dropdown', 'radiogroup'].indexOf(t) > -1) {
+                options.question.description = 'Select best answer';
+                options.question.otherText = 'Other';
+            }
+
+            if (t === 'checkbox') {
+                options.question.description = 'Check all that applies';
+            }
+            if (t === 'rating') {
+                options.question.description = 'Rating values definitions';
+
+            }
+            // if (options.property.name === "description") {
+            //     options.obj.value = 'asdasds';
+            //     options.obj.setDefaultValue()
+            //
+            // }
         });
 
         this.editor.onCanShowProperty.add((editor, options) => {
+
+            console.log(options.property.name)
+
+            if (options.property.name === 'choices') {
+
+                // options.property.visible= false
+                console.log(options)
+            }
 
             if (options.property.name === "isRequired") {
                 options.canShow = false;
@@ -94,9 +147,12 @@ class SurveyEditor extends Component {
             else {
                 options.canShow = true;
             }
+
             if (options.property.name === "visibleIf") {
                 options.property.alternativeName = 'visibleIf (coming soon)';
             }
+
+
         });
 
 
